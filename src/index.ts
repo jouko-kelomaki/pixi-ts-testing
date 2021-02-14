86fd045c2a374a2fde5c39062f466c2fe2e8b8ae
Jouko Kelomäki <jouko.kelomaki@gmail.com>
2021-02-14 16:10:06 +0200

    bundling dependencies and files with webpack set up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world")
+import * as PIXI from 'pixi.js'
+
+console.log("jou")
+
+const app: PIXI.Application = new PIXI.Application({ 
+    width: 256, 
+    height: 256,                       
+    antialias: true, 
+    transparent: false, 
+    resolution: 1
+  }
+)
+
+document.body.appendChild(app.view);
+
